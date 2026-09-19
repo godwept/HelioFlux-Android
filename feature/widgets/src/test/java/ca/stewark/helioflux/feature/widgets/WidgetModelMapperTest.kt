8a33f0e893f74d81fe210cf3b58bb7461ad1073c
godwept <mathew.stewart@gmail.com>
2026-09-19 19:58:56 -0300

    fix: construct widget test source key

--- feature/widgets/src/test/java/ca/stewark/helioflux/feature/widgets/WidgetModelMapperTest.kt
+++ feature/widgets/src/test/java/ca/stewark/helioflux/feature/widgets/WidgetModelMapperTest.kt
@@ -6,7 +6,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class WidgetModelMapperTest {
-    private val source = DataSourceKey.NOAA
+    private val source = DataSourceKey("NOAA")
 
     @Test
     fun cachedRepositoryStateMapsToCompactDisplayStringsAndCachedIndicator() {
