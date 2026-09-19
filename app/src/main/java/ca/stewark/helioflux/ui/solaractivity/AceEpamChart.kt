81ab6819b201c73ec32dc90916d2fe8d5407fd32
godwept <mathew.stewart@gmail.com>
2026-09-19 17:00:10 -0300

    fix: clean epam chart imports

--- app/src/main/java/ca/stewark/helioflux/ui/solaractivity/AceEpamChart.kt
+++ app/src/main/java/ca/stewark/helioflux/ui/solaractivity/AceEpamChart.kt
@@ -3,7 +3,6 @@ package ca.stewark.helioflux.ui.solaractivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.spacedBy
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
