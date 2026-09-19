829b85fbe97efbcf6c4d70d9458f3ba521fe7c7b
godwept <mathew.stewart@gmail.com>
2026-09-19 10:30:02 -0300

    test: add module smoke test

---
+++ core/database/src/test/java/ca/stewark/helioflux/core/database/ModuleSmokeTest.kt
@@ -0,0 +1,9 @@
+package ca.stewark.helioflux.core.database
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class ModuleSmokeTest {
+    @Test
+    fun moduleCompiles() = assertTrue(true)
+}
