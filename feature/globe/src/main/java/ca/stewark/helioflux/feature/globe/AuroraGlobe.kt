905584dc425a92a547e61d996bc94029ebbf7dd3
godwept <mathew.stewart@gmail.com>
2026-09-19 15:13:42 -0300

    fix: use SceneView Compose renderer API

--- feature/globe/src/main/java/ca/stewark/helioflux/feature/globe/AuroraGlobe.kt
+++ feature/globe/src/main/java/ca/stewark/helioflux/feature/globe/AuroraGlobe.kt
@@ -2,14 +2,9 @@ package ca.stewark.helioflux.feature.globe
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
 import io.github.sceneview.SceneView
 
 @Composable
 fun AuroraGlobe(modifier: Modifier = Modifier) {
-    AndroidView(
-        modifier = modifier,
-        factory = { context -> SceneView(context) },
-        onRelease = { sceneView -> sceneView.destroy() },
-    )
+    SceneView(modifier = modifier)
 }
