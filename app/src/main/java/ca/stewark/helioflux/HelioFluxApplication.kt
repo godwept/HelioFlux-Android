cdacd04e08e26dde4615b0db118edaa2f57c217a
godwept <mathew.stewart@gmail.com>
2026-09-19 19:30:27 -0300

    feat: start background alerts from application

--- app/src/main/java/ca/stewark/helioflux/HelioFluxApplication.kt
+++ app/src/main/java/ca/stewark/helioflux/HelioFluxApplication.kt
@@ -1,13 +1,42 @@
 package ca.stewark.helioflux
 
 import android.app.Application
+import android.content.Context
+import ca.stewark.helioflux.feature.alerts.AlertScheduler
+import ca.stewark.helioflux.feature.alerts.AlertScheduling
+import ca.stewark.helioflux.feature.alerts.AlertWorkerDependencies
+import ca.stewark.helioflux.feature.alerts.AlertWorkerDependenciesProvider
+import ca.stewark.helioflux.feature.alerts.AndroidAlertNotificationPoster
+import ca.stewark.helioflux.feature.alerts.NotificationChannels
+import ca.stewark.helioflux.feature.alerts.RepositoryAlertRepository
+import ca.stewark.helioflux.feature.alerts.RoomAlertStateStore
 
-class HelioFluxApplication : Application() {
+class HelioFluxApplication : Application(), AlertWorkerDependenciesProvider {
     lateinit var container: AppContainer
         private set
+
+    private var alertsInitialized = false
 
     override fun onCreate() {
         super.onCreate()
         container = AppContainer.create(this)
+        initializeAlerts(NotificationChannels::create, AlertScheduler(this))
     }
+
+    internal fun initializeAlerts(
+        channelCreator: (Context) -> Unit,
+        scheduler: AlertScheduling,
+    ) {
+        if (alertsInitialized) return
+        channelCreator(this)
+        scheduler.schedule()
+        alertsInitialized = true
+    }
+
+    override fun alertWorkerDependencies(): AlertWorkerDependencies =
+        AlertWorkerDependencies(
+            repository = RepositoryAlertRepository(container.spaceWeather, container.solarActivity),
+            stateStore = RoomAlertStateStore(container.database.alertStateDao()),
+            notificationPoster = AndroidAlertNotificationPoster(this),
+        )
 }
