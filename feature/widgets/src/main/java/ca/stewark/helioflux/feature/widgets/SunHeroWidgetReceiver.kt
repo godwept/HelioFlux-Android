aef8e630659079d15fa3c23a6ce2d864a656a7d8
godwept <mathew.stewart@gmail.com>
2026-09-19 20:16:38 -0300

    test: add sun widget baseline coverage

---
+++ feature/widgets/src/main/java/ca/stewark/helioflux/feature/widgets/SunHeroWidgetReceiver.kt
@@ -0,0 +1,4 @@
+package ca.stewark.helioflux.feature.widgets
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+class SunHeroWidgetReceiver:GlanceAppWidgetReceiver(){override val glanceAppWidget:GlanceAppWidget=SunHeroWidget()}
