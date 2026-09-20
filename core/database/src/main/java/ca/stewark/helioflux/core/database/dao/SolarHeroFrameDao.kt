3be67797ac3e3daa980c93df264474a65493f2b1
godwept <mathew.stewart@gmail.com>
2026-09-20 11:33:41 -0300

    feat: replace solar hero frames transactionally

--- core/database/src/main/java/ca/stewark/helioflux/core/database/dao/SolarHeroFrameDao.kt
+++ core/database/src/main/java/ca/stewark/helioflux/core/database/dao/SolarHeroFrameDao.kt
@@ -2,6 +2,7 @@ package ca.stewark.helioflux.core.database.dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Upsert
 import ca.stewark.helioflux.core.database.entity.SolarHeroFrameEntity
 import kotlinx.coroutines.flow.Flow
@@ -15,4 +16,9 @@ interface SolarHeroFrameDao {
     suspend fun latest(): SolarHeroFrameEntity?
     @Query("DELETE FROM solar_hero_frames")
     suspend fun deleteAll()
+    @Transaction
+    suspend fun replaceAll(frames: List<SolarHeroFrameEntity>) {
+        deleteAll()
+        upsertAll(frames)
+    }
 }
