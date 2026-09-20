e7099c0ba6b1b6d4b21e8edceba502f7a28f2aab
godwept <mathew.stewart@gmail.com>
2026-09-20 11:48:25 -0300

    test: update forecast presentation contract

--- app/src/test/java/ca/stewark/helioflux/ui/home/ForecastCardPresentationTest.kt
+++ app/src/test/java/ca/stewark/helioflux/ui/home/ForecastCardPresentationTest.kt
@@ -4,8 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ForecastCardPresentationTest {
-    @Test fun collapsedForecastUsesBoundedVisualLines() {
-        assertEquals(7, FORECAST_COLLAPSED_MAX_LINES)
-        assertEquals(260, FORECAST_CARD_HEIGHT_DP)
+    @Test fun collapsedForecastMatchesPwaClampAndMinimumHeight() {
+        assertEquals(5, FORECAST_COLLAPSED_MAX_LINES)
+        assertEquals(200, FORECAST_CARD_MIN_HEIGHT_DP)
     }
 }
